f3c59fca56720a699d476e32e89d668826aa56c7
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-08-18 14:53:01 +0530

    Used layer base in index.css and used list to and home , about section

--- Pro-1/src/Component/Navbar.jsx
+++ Pro-1/src/Component/Navbar.jsx
@@ -1,13 +1,36 @@
 import logo from '../assets/logo.png'
-
+import {FaBars ,FaTimes} from 'react-icons/fa'
 function Navbar(){
     return(
         <>
         <div className=" w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-        <div>
-        <img src={logo} alt='logo image' style={{width:"220px"}}></img>
+           <div>
+           <img src={logo} alt='logo image' style={{width:"220px"}}></img>
+
+            </div>
+         {/* menu */}
+           <div>
+            <ul className='flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Work</li>
+            </ul>
+           </div>
+
+         {/* hamburger */}
+
+          {/* <div>
+             <FaBars></FaBars>
+            </div> */}
+
+        {/* Social icons */}
+        <div className='hidden'>
 
         </div>
+
+
+
         </div>
         </>
     )
