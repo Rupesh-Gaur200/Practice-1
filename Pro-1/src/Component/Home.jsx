b18f87666744ae4c2cb9379f1537d8e44d251f6c
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-08-18 21:28:10 +0530

    adding rotation and view work button and using group to group the whole button

--- Pro-1/src/Component/Home.jsx
+++ Pro-1/src/Component/Home.jsx
@@ -9,14 +9,21 @@ function Home(){
         <h1 className="text-4xl font-bold text-gray-300 ">Rupesh Gaur</h1>
         <h2 className="text-4xl text-gray-200">I am Frontend Developer</h2>
          <p className="text-gray-400 py-4 ">Web Developer | Proficient in JavaScript, HTML, CSS, and Masterful with React Library | Ardent MERN Stack Enthusiast 🌐|Passionate Explorer of the IoT Domain</p>
-
+        
+         <div>
+        <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-purple-600 hover:border-purple-600">View Work 
+        
+        <span className="group-hover:rotate-90 duration-300">
+        <HiArrowNarrowRight className="ml-3 "></HiArrowNarrowRight>
+        </span>
+        
+        </button>
+      </div>
 
       </div>
 
 
-      <div>
-        <button>View Work <HiArrowNarrowRight></HiArrowNarrowRight></button>
-      </div>
+  
 
 
 
