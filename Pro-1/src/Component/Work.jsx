bb6c5527701c885a46a18c5d4aa6c417b78fccea
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-08-23 16:43:55 +0530

    added functionality  to veiw work button

--- Pro-1/src/Component/Work.jsx
+++ Pro-1/src/Component/Work.jsx
@@ -2,16 +2,15 @@
 
 function Work(){
     return(
-        <div name="Work">
-            <div className="w-full h-screen bg-[rgb(10,25,47)] text-gray-300 pt-20">
-            <div className="flex justify-start pl-56" >
+        <div name="Work" className="w-full h-screen bg-[rgb(10,25,47)] text-gray-300 pt-12">
+    
+            <div className="flex justify-start pl-56  duration-300  " >
                 
-                <div className=" cursor-pointer hover:translate-x-(-5) hover:scale-125 duration-300 sm:text-right pb-8 pl-4 pr-8">
-                <h1 className="text-4xl font-bold inline border-b-4 border-purple-600 cursor-pointer  hover:scale-125 duration-300">Work</h1>
+                <div className=" cursor-pointer hover:scale-125 duration-300 pb-8 pl-4 pr-8">
+                <h1 className=" text-4xl font-bold inline border-b-4 border-purple-600 duration-300 ">Work</h1>
             </div>
             </div>
             </div>
-        </div>
     )
 }
 export default Work
