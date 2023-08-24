662d5d52eedbfe7793ae859eb85a41e40e477441
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-08-24 15:14:45 +0530

    desgined the heading of Contact component

--- Pro-1/src/Component/Contact.jsx
+++ Pro-1/src/Component/Contact.jsx
@@ -4,10 +4,15 @@
 function Contact(){
     return (
         
-        <div name="Contact" className="w-full h-screen bg-[#0a192f] text-gray-300" >
-         <div>
-            <h1>Contact</h1>
+        <div name="Contact" className="w-full h-screen bg-[#0a192f] text-gray-300 pt-10" >
+         <div className="flex justify-evenly cursor-pointer ">
+            <h1 className="text-4xl font-bold border-b-4 border-purple-600 hover:translate-x-12 duration-300  hover:scale-125">Contact</h1>
          </div>
+              
+
+
+
+
          </div>
         
     )
