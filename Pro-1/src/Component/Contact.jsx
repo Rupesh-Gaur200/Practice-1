d71e73ce115c00acb0c61794260a8cdd327a69f8
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-08-24 15:41:26 +0530

    desgined the basic layout of contact component form

--- Pro-1/src/Component/Contact.jsx
+++ Pro-1/src/Component/Contact.jsx
@@ -4,13 +4,24 @@
 function Contact(){
     return (
         
-        <div name="Contact" className="w-full h-screen bg-[#0a192f] text-gray-300 pt-10" >
-         <div className="flex justify-evenly cursor-pointer ">
-            <h1 className="text-4xl font-bold border-b-4 border-purple-600 hover:translate-x-12 duration-300  hover:scale-125">Contact</h1>
+        <div name="Contact" className="w-full h-screen bg-[#0a192f] text-gray-300 pt-10 " >
+         <div className="flex justify-evenly cursor-pointer  ">
+            <h1 className="text-4xl font-bold border-b-4 border-purple-600 hover:translate-x-12 duration-300  hover:scale-125">Connect With Me</h1>
          </div>
+
+         <div className="flex justify-center pt-24 ">
               
+              <div className="w-[800px] h-[500px] border-8 border-purple-600 flex justify-center pr-8 ">
+
+                <form>
+                    <label></label>
 
 
+
+
+                </form>
+              </div>
+              </div>
 
 
          </div>
