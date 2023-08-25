c4aac2602cd2746c23dcb278af7c5a0d0d202edd
rupeshgaur2001 <rupeshgaurr200@gmail.com>
2023-08-25 20:49:09 +0530

    completed the layouting and desging of form in contact component

--- Pro-1/src/Component/Contact.jsx
+++ Pro-1/src/Component/Contact.jsx
@@ -6,20 +6,27 @@ function Contact(){
         
         <div name="Contact" className="w-full h-screen bg-[#0a192f] text-gray-300 pt-10 " >
          <div className="flex justify-evenly cursor-pointer  ">
-            <h1 className="text-4xl font-bold border-b-4 border-purple-600 hover:translate-x-12 duration-300  hover:scale-125">Connect With Me</h1>
+            <h1 className="text-4xl font-bold border-b-4 border-purple-600 duration-300  hover:scale-125">Connect With Me</h1>
          </div>
 
          <div className="flex justify-center pt-24 ">
               
               <div className="w-[800px] h-[500px] border-8 border-purple-600 flex justify-center pr-8 ">
-
-                <form>
-                    <label></label>
+                 <div className=" border-2 border-purple-600 w-[625px] h-[450px] ml-8 mt-4">
+                <form className=" flex flex-col gap-8">
+                    
+                    <input className="ml-3 mt-3  mr-3  h-8 pl-3"type="text" value="Name"></input>
+                    <input className="ml-3 mt-3 mr-3  h-8 pl-3" type="text" value="Email"></input>
+                    <textarea  className="ml-3 mt-3 mr-3 pl-3  h-[200px]"type="text" value="please drop your message here"></textarea>
+                     
+                      <button className="border-2 border-white w-[200px] ml-52 h-10 ">Let's Collabrate</button>
+                   
 
 
 
 
                 </form>
+                </div>
               </div>
               </div>
 
